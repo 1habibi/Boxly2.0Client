112b9a2a53bb294725cfd02a96837fc48f29e8cc
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-02-22 09:45:41 +0300

    fix: fix updating refresh token

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,5 +1,9 @@
 import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
-import { logOut, setCredentials } from "@/features/auth/authSlice.js";
+import {
+  logOut,
+  setCredentials,
+  setTokenRefreshing,
+} from "@/features/auth/authSlice.js";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
@@ -17,21 +21,21 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
 
-  if (
-    result?.error?.status === 401 &&
-    args.url !== "/auth/login" &&
-    args.url !== "/user"
-  ) {
+  if (result?.error?.status === 401 && args.url !== "/auth/login") {
     console.log("sending refresh token");
     // send refresh token to get new access token
-    const refreshResult = await baseQuery(
-      "/auth/refresh-tokens",
-      api,
-      extraOptions
-    );
+    const refreshResult = await baseQuery("/auth/refresh-tokens", api, {
+      ...extraOptions,
+      onQueryStarted: (arg, { dispatch }) => {
+        dispatch(setTokenRefreshing(true));
+      },
+      onSuccess: (data, arg, { dispatch }) => {
+        // Установить состояние загрузки после получения ответа
+        dispatch(setTokenRefreshing(false));
+      },
+    });
     console.log("refresh result: ", refreshResult);
     if (refreshResult?.data) {
-      // store the new token
       api.dispatch(setCredentials({ ...refreshResult.data }));
       sessionStorage.setItem("accessToken", refreshResult.data.accessToken);
       result = await baseQuery(args, api, extraOptions);
