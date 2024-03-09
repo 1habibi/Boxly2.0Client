ccaaa343ccee571e5ab10c477c3ff1a571270fc5
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-03-09 10:50:48 +0300

    feat: make responsive AppMenu

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,29 +1,60 @@
-import { Layout } from "antd";
+import { Button, Drawer, Layout } from "antd";
 const { Header, Footer, Content } = Layout;
 import s from "./App.module.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Logo } from "@/copmonents/Logo/Logo.jsx";
 import { AppMenu } from "@/copmonents/AppMenu/AppMenu.jsx";
 import { LoginGroup } from "@/copmonents/LoginGroup/LoginGroup.jsx";
 import { routes } from "@/router/index.jsx";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Layout className={s.appLayout}>
       <Header className={s.header}>
         <Logo></Logo>
-        <AppMenu></AppMenu>
-        <LoginGroup></LoginGroup>
+        {windowWidth < 768 ? (
+          <>
+            <Button type="primary" onClick={showDrawer}>
+              ☰
+            </Button>
+            <Drawer
+              title={<LoginGroup></LoginGroup>}
+              placement="left"
+              onClose={onClose}
+              open={visible}
+            >
+              <AppMenu mode={"vertical"}></AppMenu>
+            </Drawer>
+          </>
+        ) : (
+          <>
+            <AppMenu></AppMenu>
+            <LoginGroup></LoginGroup>
+          </>
+        )}
       </Header>
-      <Content
-        style={{
-          backgroundColor: "lightblue",
-          height: "100vh",
-        }}
-      >
-        {routes()}
-      </Content>
-      <Footer>FOOTER</Footer>
+      <Content className={s.content}>{routes()}</Content>
+      <Footer className={s.footer}>Boxly ©2024 Created by habibi</Footer>
     </Layout>
   );
 }
