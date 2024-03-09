b21ca2ce3c308f216b100ee388f7084cbfdacc51
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-03-09 10:48:54 +0300

    feat: update router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,11 +4,11 @@ import { Home } from "../pages/Home/Home.jsx";
 import { Login } from "../pages/Login/Login.jsx";
 import { Register } from "../pages/Register/Register.jsx";
 import { NotFound } from "../pages/NotFound/NotFound.jsx";
-import { UserProfile } from "@/pages/UserProfile/UserProfile.jsx";
 import { RequireAuth } from "@/utils/RequireAuth/RequireAuth.jsx";
 import { Welcome } from "@/pages/Welcome/Welcome.jsx";
 import { MeTest } from "@/pages/MeTest/MeTest.jsx";
 import { Profile } from "@/pages/Profile/Profile.jsx";
+import { MyOrders } from "@/pages/Orders/MyOrders/MyOrders.jsx";
 
 export const PATH = {
   HOME: "/",
@@ -17,6 +17,7 @@ export const PATH = {
   USER_PROFILE: "/profile",
   WELCOME: "/welcome",
   USER_ME: "/me",
+  MY_ORDERS: "/orders",
 };
 
 export const routes = () => (
@@ -24,12 +25,12 @@ export const routes = () => (
     <Route path={"/"} element={<Home />}></Route>
     <Route path={"/login"} element={<Login />}></Route>
     <Route path={"/register"} element={<Register />}></Route>
-    <Route path={"/user-profile"} element={<UserProfile />}></Route>
     <Route path={"*"} element={<NotFound />}></Route>
     <Route element={<RequireAuth />}>
       <Route path={"/welcome"} element={<Welcome />}></Route>
       <Route path={"/me"} element={<MeTest />}></Route>
       <Route path={"/profile"} element={<Profile />}></Route>
+      <Route path={"/orders"} element={<MyOrders />}></Route>
     </Route>
   </Routes>
 );
