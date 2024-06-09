4693a8c1d3cec446088957e82a2a4d09d6dd1abe
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-06-09 20:05:27 +0300

    refactor: update jsconfig & vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import path from "path";
 import { ngrok } from "vite-plugin-ngrok";
 
 export default defineConfig({
-  plugins: [react(), ngrok("EXAMPLE")],
+  plugins: [
+    react(),
+    ngrok("2d0ajkYt3S41bN0cVIuQbYB9F0N_59bfdCUmqD92d32BCgihh"),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
