694e67fa178143ecfec777d84e6ca83f542019eb
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-06-09 20:10:40 +0300

    feat: update LoadAuth

--- src/utils/LoadAuth/LoadAuth.jsx
+++ src/utils/LoadAuth/LoadAuth.jsx
@@ -1,65 +1,64 @@
-import React, { useEffect, useState } from "react";
 import {
-  selectCurrentUser,
-  selectIsTokenRefreshing,
-  setCredentials,
-} from "@/features/auth/authSlice.js";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  useGetCurrentProfileQuery,
-  useGetCurrentUserQuery,
+	useGetCurrentProfileQuery,
+	useGetCurrentUserQuery
 } from "@/features/auth/authApiSlice.js";
-import { Flex, Spin } from "antd";
+import {
+	selectIsTokenRefreshing,
+	setCredentials
+} from "@/features/auth/authSlice.js";
+import { Spin } from "antd";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export const LoadAuth = ({ children }) => {
-  const dispatch = useDispatch();
-  const isTokenRefreshing = useSelector(selectIsTokenRefreshing);
+	const dispatch = useDispatch();
+	const isTokenRefreshing = useSelector(selectIsTokenRefreshing);
 
-  useEffect(() => {
-    const accessToken = sessionStorage.getItem("accessToken");
-    if (accessToken) {
-      dispatch(setCredentials({ accessToken }));
-    }
-  }, [dispatch]);
+	useEffect(() => {
+		const accessToken = sessionStorage.getItem("accessToken");
+		if (accessToken) {
+			dispatch(setCredentials({ accessToken }));
+		}
+	}, [dispatch]);
 
-  const {
-    data: user,
-    isLoading: isUserLoading,
-    isError: isUserError,
-  } = useGetCurrentUserQuery(undefined, {
-    skip: !sessionStorage.getItem("accessToken"),
-  });
-  const {
-    data: profile,
-    isLoading: isProfileLoading,
-    isError: isProfileError,
-  } = useGetCurrentProfileQuery(user?.id, {
-    skip: !user,
-  });
+	const {
+		data: user,
+		isLoading: isUserLoading,
+		isError: isUserError
+	} = useGetCurrentUserQuery(undefined, {
+		skip: !sessionStorage.getItem("accessToken")
+	});
+	const {
+		data: profile,
+		isLoading: isProfileLoading,
+		isError: isProfileError
+	} = useGetCurrentProfileQuery(user?.id, {
+		skip: !user
+	});
 
-  const isLoading = isUserLoading || isProfileLoading || isTokenRefreshing;
-  const hasError = isUserError || isProfileError;
+	const isLoading = isUserLoading || isProfileLoading || isTokenRefreshing;
+	const hasError = isUserError || isProfileError;
 
-  if ((hasError || !profile || !user) && !isLoading) {
-    // Handle the case when user or profile data is not available
-    return children;
-  }
+	if ((hasError || !profile || !user) && !isLoading) {
+		// Handle the case when user or profile data is not available
+		return children;
+	}
 
-  if (isLoading)
-    return (
-      <div>
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Spin size="large" />
-        </div>
-      </div>
-    );
+	if (isLoading)
+		return (
+			<div>
+				<div
+					style={{
+						height: "100vh",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center"
+					}}
+				>
+					<Spin size="large" />
+				</div>
+			</div>
+		);
 
-  return children;
+	return children;
 };
