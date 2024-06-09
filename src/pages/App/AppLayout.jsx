2f3aae1f03e19adc306af2537a58c17b4217619b
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-06-09 20:09:55 +0300

    feat: add appLayout

---
+++ src/pages/App/AppLayout.jsx
@@ -0,0 +1,64 @@
+import { AppMenu } from "@/copmonents/AppMenu/AppMenu";
+import { LoginGroup } from "@/copmonents/LoginGroup/LoginGroup.jsx";
+import { Logo } from "@/copmonents/Logo/Logo.jsx";
+import { Button, Drawer, Layout } from "antd";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import s from "./App.module.scss";
+const { Header, Footer, Content } = Layout;
+
+function AppLayout() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [visible, setVisible] = useState(false);
+
+	const showDrawer = () => {
+		setVisible(true);
+	};
+
+	const onClose = () => {
+		setVisible(false);
+	};
+
+	useEffect(() => {
+		const handleResize = () => setWindowWidth(window.innerWidth);
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	return (
+		<Layout className={s.appLayout}>
+			<Header className={s.header}>
+				<Logo></Logo>
+				{windowWidth < 768 ? (
+					<>
+						<Button type="primary" onClick={showDrawer}>
+							☰
+						</Button>
+						<Drawer
+							title={<LoginGroup></LoginGroup>}
+							placement="left"
+							onClose={onClose}
+							open={visible}
+						>
+							<AppMenu mode={"vertical"}></AppMenu>
+						</Drawer>
+					</>
+				) : (
+					<>
+						<AppMenu></AppMenu>
+						<LoginGroup></LoginGroup>
+					</>
+				)}
+			</Header>
+			<Content className={s.content}>
+				<Outlet />
+			</Content>
+			<Footer className={s.footer}>Boxly ©2024 Created by habibi</Footer>
+		</Layout>
+	);
+}
+
+export default AppLayout;
