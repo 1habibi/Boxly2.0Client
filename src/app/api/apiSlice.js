3b2499c15e868a42e1a24afeb790dd39f57e524e
Bogdan Migunov <migunov.bbbb@gmail.com>
2024-06-09 20:04:31 +0300

    feat: update apiSlice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,58 +1,54 @@
-import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import {
-  logOut,
-  setCredentials,
-  setTokenRefreshing,
+	logOut,
+	setCredentials,
+	setTokenRefreshing
 } from "@/features/auth/authSlice.js";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
-  credentials: "include",
-  prepareHeaders: (headers, { getState }) => {
-    // const token = getState().auth.token;
-    const token = sessionStorage.getItem("accessToken");
-    if (token) {
-      headers.set("authorization", `${token}`);
-    }
-    return headers;
-  },
+	baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
+	credentials: "include",
+	prepareHeaders: (headers, { getState }) => {
+		const token = sessionStorage.getItem("accessToken");
+		if (token) {
+			headers.set("authorization", `${token}`);
+		}
+		return headers;
+	}
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions);
-  const accessToken = sessionStorage.getItem("accessToken");
+	let result = await baseQuery(args, api, extraOptions);
+	const accessToken = sessionStorage.getItem("accessToken");
 
-  if (
-    result?.error?.status === 401 &&
-    args.url !== "/auth/login" &&
-    accessToken
-  ) {
-    console.log("sending refresh token");
-    // send refresh token to get new access token
-    const refreshResult = await baseQuery("/auth/refresh-tokens", api, {
-      ...extraOptions,
-      onQueryStarted: (arg, { dispatch }) => {
-        dispatch(setTokenRefreshing(true));
-      },
-      onSuccess: (data, arg, { dispatch }) => {
-        // Установить состояние загрузки после получения ответа
-        dispatch(setTokenRefreshing(false));
-      },
-    });
-    console.log("refresh result: ", refreshResult);
-    if (refreshResult?.data) {
-      api.dispatch(setCredentials({ ...refreshResult.data }));
-      sessionStorage.setItem("accessToken", refreshResult.data.accessToken);
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logOut());
-    }
-  }
-  return result;
+	if (
+		result?.error?.status === 401 &&
+		args.url !== "/auth/login" &&
+		accessToken
+	) {
+		const refreshResult = await baseQuery("/auth/refresh-tokens", api, {
+			...extraOptions,
+			onQueryStarted: (arg, { dispatch }) => {
+				dispatch(setTokenRefreshing(true));
+			},
+			onSuccess: (data, arg, { dispatch }) => {
+				dispatch(setTokenRefreshing(false));
+			}
+		});
+		console.log("refresh result: ", refreshResult);
+		if (refreshResult?.data) {
+			api.dispatch(setCredentials({ ...refreshResult.data }));
+			sessionStorage.setItem("accessToken", refreshResult.data.accessToken);
+			result = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(logOut());
+		}
+	}
+	return result;
 };
 
 export const apiSlice = createApi({
-  baseQuery: baseQueryWithReauth,
-  tagTypes: ["Order", "Profile"],
-  endpoints: (builder) => ({}),
+	baseQuery: baseQueryWithReauth,
+	tagTypes: ["Order", "Profile", "User"],
+	endpoints: builder => ({})
 });
